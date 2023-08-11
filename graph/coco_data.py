7554cb937ff9e531b7126cac8b9411bbbf53f310
JIA-ZHANG666 <53674114+JIA-ZHANG666@users.noreply.github.com>
2023-08-11 12:10:39 +0800

    Update coco_data.py

--- graph/coco_data.py
+++ graph/coco_data.py
@@ -1,11 +1,14 @@
 import pickle
+import json
 import numpy as np
 
-def get_coco_data():
+def get_voc_data():
     print('obtaining coco data ...')
-    with open("graph/adj_mat_coco.pickle","rb") as f:
-        graph_adj_mat = pickle.load(f)
-
+    with open("Semantic Consistency/Stored matrices/CM_kg_57_info.json","rb") as f:
+        info = json.load(f)
+        KG_COCO_info = info['KG_COCO_info']
+        graph_adj_mat = np.asarray(KG_COCO_info['S'])
+    
         print('the adj mat is\n',graph_adj_mat)
         print('the type is\n', type(graph_adj_mat))
         print('the shape is\n', graph_adj_mat.shape)
@@ -13,8 +16,7 @@ def get_coco_data():
 
         num_symbol_node = graph_adj_mat.shape[0]
 
-
-    with open("graph/embed_mat_cocov2_300.pickle","rb") as f:
+    with open("graph/coco_glove_word2vec.pkl","rb") as f:
         fasttest_embeddings = pickle.load(f)
         fasttest_dim = fasttest_embeddings.shape[1]
 
@@ -23,13 +25,10 @@ def get_coco_data():
         print('the shape is\n', fasttest_embeddings.shape)
         print('nonzero\n', np.count_nonzero(fasttest_embeddings))
 
-    print('obtained coco data')
+    print('obtained voc data')
 
     return {"num_symbol_node":num_symbol_node,
             "fasttest_embeddings":fasttest_embeddings,
              "fasttest_dim": fasttest_dim,
             "graph_adj_mat": graph_adj_mat
             }
-
-
-
